db870e58297d74ca0ccfc13f7318e7aae87ef6c4
T4rm1nal-L1nux <68395530+truck164@users.noreply.github.com>
2021-07-27 14:58:03 +0800

    Run this
    This is the main file

---
+++ WifiNulls.py
@@ -0,0 +1,96 @@
+import time
+print("\033[1;32;40m                       ====================")
+print("\033[1;32;40m                       =====\033[1;32;36m[\033[1;32;35mWifi Null\033[1;32;40m\033[1;32;36m]\033[1;32;40m====")
+print("\033[1;32;40m                       ====================")
+print("\033[1;32;39m Starting...")
+print("\033[1;32;34m                     [x]Made By Cyber_Ant [x]")
+print("\033[1;32;34m                    [x]From Team CyberNulls [x]")
+time.sleep(2)
+print()
+print("\033[1;32;31m")
+print("Turn on Your wifi and look at the advance option to see proxy and bypass and oder")
+time.sleep(5)
+
+print()
+
+print("Descliamer for education purposes only")
+time.sleep
+print()
+print("This tool will be illegal if you use this in oder wifi without permission.")
+time.sleep(4)
+print()
+
+pkg = input("\033[1;32;40mYou need to install some pkg first? (y/n)")
+time.sleep(1)
+print()
+
+print("\033[1;32;40mInstalling some pkg...")
+time.sleep(4)
+print()
+print("Done")
+time.sleep(1)
+print()
+
+print("Installing Wifi finder...")
+time.sleep(2)
+print()
+print("Done")
+time.sleep
+print()
+WifiName = input("\033[1;32;35m[Virtual][$Root]Wifi Name: ")
+time.sleep(2)
+print()
+
+ip = input("\033[1;32;35m[Virtual][$Root] Wifi Ip: ")
+time.sleep(2)
+print()
+
+proxy = input("\033[1;32;35m[Virtual][$Root]Proxy port: ")
+time.sleep(2)
+print()
+
+bypass = input("\033[1;32;35m[Virtual][$Root]Bypass proxy (y\n): ")
+time.sleep(3)
+print()
+
+Gateway = input("\033[1;32;35m[Virtual][$Root]Gateway:")
+time.sleep(3)
+print()
+
+print("Hacking...")
+time.sleep(50)
+print()
+
+print("\033[1;32;35m wifi name :" + WifiName)
+time.sleep(3)
+print()
+
+print("\033[1;32;35m Wifi ip:" + ip)
+time.sleep(5)
+print()
+
+print("\033[1;32;35mProxy: " + proxy)
+time.sleep(4)
+print()
+
+print("\033[1;32;35mBypass: " + bypass)
+time.sleep(2)
+print()
+
+print("\033[1;32;35mGateway: " + Gateway)
+time.sleep(3)
+print()
+
+print("\033[1;32;35mFinding password...")
+time.sleep(20)
+
+
+print("\033[1;32;35mPassword found")
+time.sleep(5)
+
+print("\033[1;32;35mPassword xploited")
+print("<a href:password ehrhfydhhr8274 crack the hash using hash cracker>click</a> ")
+print(" copy it and Use Html to Show the password")
+time.sleep(1)
+print("\033[1;32;34m Hacked by CyberNulls")
+
